95f15ebe99befd1e641e8065e72ae625cc41c649
1 <1>
2022-03-17 12:37:32 +0300

    отредактирована ошибка работы каналов

--- main.go
+++ main.go
@@ -11,8 +11,7 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-	var total int // общее число вхлождений
-	k := 5        // max количество запущенных горутин, дано в условии задачи
+	k := 5 // max количество запущенных горутин, дано в условии задачи
 	var urls = []string{
 		"https://golang.org",
 		"https://go.dev/",
@@ -65,35 +64,46 @@ func main() {
 	}
 
 	countGo := make(chan int, k) // указываем длинну канала (ссылка 1)
+	c := make(chan int, k)       //для счетчика всех вхождений
+	var total int
 
-	c := make(chan int, k) //для счетчика всех вхождений
+	go func() {
+		for count := range c {
+			//fmt.Println("count c", count)
+			total = total + count
+		}
+	}()
 
 	for i, url := range urls {
+		wg.Add(1)
 		countGo <- i // ссылка 1 -  так как у нас фиксированная длинна канала,
 		// то при заполнении канала будем ожидать когда хотя бы один из 5ти элементов
 		//освободится иначе программа заблокирована
-
-		go CountGo(countGo, c, url, &wg)
-
-		total = total + <-c
+		go CountGo(c, countGo, url, &wg)
+	}
+	wg.Wait()
+	for {
+		if len(c) == 0 {
+			fmt.Println("Total:", total)
+			return
+		}
 
 	}
 
-	wg.Wait()
-	fmt.Println("Total:", total)
 }
 
-func CountGo(countGo chan int, c chan int, url string, wg *sync.WaitGroup) {
-	wg.Add(1)
+func CountGo(c chan int, countGo chan int, url string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err.Error())
+		return
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err.Error())
+		return
 	}
 
 	srtForCount := string(body)
@@ -102,6 +112,8 @@ func CountGo(countGo chan int, c chan int, url string, wg *sync.WaitGroup) {
 	count := strings.Count(srtForCount, "go")
 
 	fmt.Println("Count for", url, ": ", count)
+
 	c <- count
 	<-countGo
+
 }
